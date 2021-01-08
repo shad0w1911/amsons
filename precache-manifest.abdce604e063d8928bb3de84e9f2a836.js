bbdd173e80a38aee4b061e4fe4f9a325a353a0cd
Hassaan Uddin Sheikh <unbundleracers@gmail.com>
2021-01-08 19:46:37 +0500

    Updates

---
+++ precache-manifest.abdce604e063d8928bb3de84e9f2a836.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3a0b87837f0cc06edae7686df8761a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "624eaef700f47197f206",
+    "url": "/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "4b97a81621125d14afae",
+    "url": "/static/js/2.eaba89bf.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.eaba89bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624eaef700f47197f206",
+    "url": "/static/js/main.d77da460.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "d590d5ee83ce78975862b5ad2b28d86f",
+    "url": "/static/media/contactBg.d590d5ee.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "3460b8fd340e34b4f4d2087be4b446c6",
+    "url": "/static/media/header.3460b8fd.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
